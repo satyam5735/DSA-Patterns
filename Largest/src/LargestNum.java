9569d86a29854a17d2f0670f6cc0adff7b250171
Satyam <pmishra2425@gmail.com>
2026-02-15 22:53:55 +0530

    Initial Commit

---
+++ Largest/src/LargestNum.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class LargestNum {
+    public static int largestNum(int numbers[]) {
+        int largest = Integer.MIN_VALUE;
+
+        for(int i=0;i<numbers.length;i++){
+            if(numbers[i]>largest){
+                largest = numbers[i];
+            }
+        }
+        return largest;
+    }
+    public static void main(String[] args) {
+        int numbers[] = {1,2,3,4,5,6,7};
+        System.out.println(largestNum(numbers));
+    }
+}
